98e6e7b3e96b59693b8038c60ea8549b0bc5505f
Andrew Bishop <abishop195@gmail.com>
2019-07-09 06:42:29 +0000

    Cleanup of setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ WEB_CONTAINER_NAME=sampleweb
 install_docker() {
 	if ! [ -x "$(command -v docker)" ]; then
 	  echo 'installing docker'
-	  sudo apt-get update
-	  sudo apt-get install docker.io
+	  apt-get update
+	  apt-get install docker.io
 	fi
 }
 
@@ -13,7 +13,7 @@ install_docker() {
 check_for_container() {
 	#to use call check_for_container <name of container>
 	result=$( sudo docker ps -f name=$1 )
-	if [[ -n "$result" ]]; then
+	if [ -n "$result" ]; then
 		return 1
 	else
 		return 0
@@ -22,33 +22,40 @@ check_for_container() {
 
 remove_container() {
 	#to use call remove_container <name of container>
-	sudo docker rm --force $1
+	#removes container if it exists
+	check_for_container $1
+	if [ $? -eq 1 ]; then
+		docker rm --force $1
+	fi
 }
 
 
 setup_db() {
-	check_for_container $DB_CONTAINER_NAME
-	if [ $? -eq 1 ]; then
-		remove_container $DB_CONTAINER_NAME
-	fi
-	echo "Creating container"
-	sudo docker run --name $DB_CONTAINER_NAME -e POSTGRES_PASSWORD=hello -d postgres
-	#todo setup data in db
+	echo "Removing previous db docker containers"
+	remove_container $DB_CONTAINER_NAME
+	echo "Create new db docker container"
+	docker run --name $DB_CONTAINER_NAME -e POSTGRES_PASSWORD=hello -d postgres
 }
 
 
 setup_web() {
-	check_for_container $WEB_CONTAINER_NAME
-	if [ $? -eq 1 ]; then
-		remove_container $WEB_CONTAINER_NAME
-	fi
-	echo "Creating container"
-	pushd sample_web
-	sudo docker build -t flasksample .
-	sudo docker run --name $WEB_CONTAINER_NAME -d flasksample
-	popd
+	DOCKER_IMAGE=flasksample:latest
+	remove_container $WEB_CONTAINER_NAME
+	cd sample_web
+	docker build -q -t $DOCKER_IMAGE .
+	docker run -d -p 5000:5000 --name $WEB_CONTAINER_NAME $DOCKER_IMAGE
+	cd ..
 }
 
+
+#---- Script execution starts here ----
+
+
+if [ $(id -u) -ne 0 ]; then 
+	printf 'Please run command as follows\n\tsudo setup.sh\n'
+	exit 1
+fi
+pwd
 install_docker
 setup_db
 setup_web
